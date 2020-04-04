5a42bf8e4cd75155ad6e687f5a28da7cbc569f4b
ethan-t-obrien <ethanobrien1009@gmail.com>
2020-04-04 15:00:25 +1300

    learning hooks back to class

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Ethan', age: 28 },
+      { name: 'Brutus', age: 10},
+      { name: 'Rauts', age: 22}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!')
+    // Don't do this: this.state.persons[0].name = 'Ethan Tamati'
+    this.setState({
+      persons: [
+        { name: 'Ethan Tamati', age: 28 },
+        { name: 'Brutus', age: 10},
+        { name: 'Rauts', age: 26}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
       </div>
     );
   }
